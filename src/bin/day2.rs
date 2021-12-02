fb2b2fd27467a2cebb97243499298761c2b2cbae
Aaron Lengyel <aaron.lengyel@ucalgary.ca>
2021-12-02 03:09:32 -0700

    Second Star

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -3,7 +3,22 @@ use std::io::{stdin, stdout, BufWriter, Read, Stdout, Write};
 
 #[allow(non_snake_case)]
 fn solve<R: Read>(mut scan: Scanner<R>, mut out: BufWriter<Stdout>) -> Result<(), StopCode> {
-    //
+    let words = scan.words::<u16>().collect::<Vec<_>>();
+    writeln!(
+        out,
+        "{}",
+        words
+            .windows(3)
+            .fold((-1, 0), |(count, prev), w| {
+                let sum = w.into_iter().sum();
+                if prev < sum {
+                    (count + 1, sum)
+                } else {
+                    (count, sum)
+                }
+            })
+            .0
+    )?;
     Ok(out.flush()?)
 }
 
